3b39e448f061c4dea2db96743f94a7f56dce0dbf
NhatTruong-dotnet <builehoangnhattruong@gmail.com>
2022-03-02 18:18:02 +0700

    fix error guest not create conversations

--- routes/conversations.js
+++ routes/conversations.js
@@ -18,13 +18,24 @@ router.post("/:gmail", async (req, res) => {
       gmail: req.body.gmail,
       messageText: req.body.messageText,
     };
-    var savedConversation = await Conversation.updateOne(
-      {
-        gmail: req.params.gmail,
-      },
-      { $push: { messages: newMessage } }
-    );
-    res.status(200).json(savedConversation);
+    const conversation = await Conversation.find({
+      gmail: req.params.gmail,
+    });
+    if(conversation.length != 0){
+      var savedConversation = await Conversation.updateOne(
+        {
+          gmail: req.params.gmail,
+        },
+        { $push: { messages: newMessage } }
+      );
+      console.log('updated');
+      res.status(200).json(savedConversation);
+    }
+    else{
+      console.log('insert');
+      var newConversations = await Conversation.insertMany(newMessage);
+      res.status(200).json(newConversations);
+    }
   } catch (error) {
     res.status(500).json(error);
     console.log(error);
