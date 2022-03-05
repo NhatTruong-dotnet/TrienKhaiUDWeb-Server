1c9bab2c92772dacf9607e8ac918f24d70ac9dd4
NhatTruong-dotnet <builehoangnhattruong@gmail.com>
2022-03-05 10:11:13 +0700

    update get all books

--- routes/Books.js
+++ routes/Books.js
@@ -12,7 +12,8 @@ router.get("/:name", async (req, res) => {
     res.status(500).json(err);
   }
 });
-router.post("/", async (req, res) => {
+
+router.get("/", async (req, res) => {
   try {
       const books = await Book.find({});
       
@@ -23,6 +24,16 @@ router.post("/", async (req, res) => {
   }
 });
 
-
+router.get("/review/:name", async (req, res) => {
+  try {
+      const books = await Book.find({
+        name: req.params.name
+      });
+      res.status(200).json(books); 
+    }
+ catch (error) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
