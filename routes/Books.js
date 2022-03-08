f8d0b4d1320d7c7bfdc69cd2882b90511d5f747b
Thanhthuan020 <71782129+cabupig036@users.noreply.github.com>
2022-03-08 22:31:32 +0700

    GetIdNew

--- routes/Books.js
+++ routes/Books.js
@@ -7,7 +7,6 @@ router.get("/:name", async (req, res) => {
         name: req.params.name
         
       });
-      console.log(books.rating);
       res.status(200).json(books); 
 
     }
@@ -15,15 +14,10 @@ router.get("/:name", async (req, res) => {
     res.status(500).json(err);
   }
 });
-router.get("/:_id", async (req, res) => {
+router.get("/getID/:_id", async (req, res) => {
   try {
-      const books = await Book.find({
-        _id: req.params._id
-        
-      });
-      console.log(books.rating);
+      const books = await Book.findOne({_id: req.params._id });
       res.status(200).json(books); 
-
     }
  catch (error) {
     res.status(500).json(err);
@@ -45,19 +39,6 @@ router.get("/delete/:_id", async (req, res) => {
   try {
       const books = await Book.deleteOne({_id: req.params._id });
       console.log("Deleted");
-      res.status(200).json(books); 
-    }
- catch (error) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/insert/", async (req, res) => {
-  try {
-      const books = await Book.insertOne({
-        name: req.body.name ,
-      });
-      console.log("Inserted");
       res.status(200).json(books); 
     }
  catch (error) {
@@ -115,42 +96,42 @@ router.put("/updateBook/:_id", async (req, res) => {
   }
 });
 
-router.get("/CreateBook/:_id", async (req, res) => {
-  try {
-    Book.findOne({
-      _id: req.params._id
-    }).exec((err, book) => {
-      if (err) {
-        res.status(500).json({message:"Update Failed"});
-      } else {
-        book.name = req.body.name;
-        book.publisher = req.body.publisher;
-        book.suppiler = req.body.suppiler;
-        book.numberInStock = req.body.numberInStock;
-        book.numberDelivery = req.body.numberDelivery;
+// router.get("/CreateBook/:_id", async (req, res) => {
+//   try {
+//     Book.findOne({
+//       _id: req.params._id
+//     }).exec((err, book) => {
+//       if (err) {
+//         res.status(500).json({message:"Update Failed"});
+//       } else {
+//         book.name = req.body.name;
+//         book.publisher = req.body.publisher;
+//         book.suppiler = req.body.suppiler;
+//         book.numberInStock = req.body.numberInStock;
+//         book.numberDelivery = req.body.numberDelivery;
 
-        book.rating[req.body.index].gmail = req.body.gmail;
-        book.rating[req.body.index].ratingValue = req.body.ratingValue;
-        book.rating[req.body.index].ratingDate = req.body.ratingDate;
-        book.rating[req.body.index].commentText = req.body.commentText;
+//         book.rating[req.body.index].gmail = req.body.gmail;
+//         book.rating[req.body.index].ratingValue = req.body.ratingValue;
+//         book.rating[req.body.index].ratingDate = req.body.ratingDate;
+//         book.rating[req.body.index].commentText = req.body.commentText;
 
-        book.author = req.body.author;
-        book.translator = req.body.translator;
-        book.publishYear = req.body.publishYear;
-        book.bookLayout = req.body.bookLayout;
-        book.price = req.body.price;
-        book.quantityOfPage = req.body.quantityOfPage;
-        book.describe = req.body.describe;
-        book.updatedAt = req.body.updatedAt;
-        book.profilePicture = req.body.picture;
-        book.save();
-        return res.status(200).json({
-          message: "Update Completely"
-        })
-      }
-    });
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+//         book.author = req.body.author;
+//         book.translator = req.body.translator;
+//         book.publishYear = req.body.publishYear;
+//         book.bookLayout = req.body.bookLayout;
+//         book.price = req.body.price;
+//         book.quantityOfPage = req.body.quantityOfPage;
+//         book.describe = req.body.describe;
+//         book.updatedAt = req.body.updatedAt;
+//         book.profilePicture = req.body.picture;
+//         book.save();
+//         return res.status(200).json({
+//           message: "Update Completely"
+//         })
+//       }
+//     });
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
 module.exports = router;
