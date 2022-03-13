d621a92c55b95b0e6b656e4af81053275edb4385
NhatTruong-dotnet <builehoangnhattruong@gmail.com>
2022-03-13 16:52:36 +0700

    fix error not getting on delivery

--- routes/bill.js
+++ routes/bill.js
@@ -158,15 +158,18 @@ router.post("/", async (req, res) => {
 router.get("/status/:status", async (req, res) => {
   try {
     if (req.params.status === "all") {
+      console.log('tat ca');
       const bill = await Bills.find();
       res.status(200).json(bill);
     } else if (req.params.status === "success") {
+      console.log('giao thanh cong');
       const bill = await Bills.find({ isDelivery: true, isSucessful: true });
       res.status(200).json(bill);
     } else if (req.params.status === "fail") {
       const bill = await Bills.find({ isDelivery: true, isSucessful: false });
       res.status(200).json(bill);
-    } else {
+    } else if(req.params.status === "onway"){
+      console.log('dang giao');
       const bill = await Bills.find({ isDelivery: false});
       res.status(200).json(bill);
     }
