4fa2412119390a1f4da4cc7c470d3331752c953f
SuongNguyenThanh <ntsuong0309@gmail.com>
2022-03-14 23:56:16 +0700

    add filed createAt in addPointLogs

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const shippingAdressSchema = new mongoose.Schema({ isDefault: { type: Boolean, default: false }, address: String });
-const addedPointLogSchema = new mongoose.Schema({ billID: String, addedPoint: Number });
+const addedPointLogSchema = new mongoose.Schema({ billID: String, addedPoint: Number, createAt: { type: Date, default: Date.now } });
 const seenItemSchema = new mongoose.Schema({ bookId: String, price: Number, amount: Number, bookName: String });
 const wishItemSchema = new mongoose.Schema({ bookId: String });
 const UserSchema = new mongoose.Schema({
