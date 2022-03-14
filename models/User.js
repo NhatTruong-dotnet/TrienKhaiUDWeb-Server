2a4c12a45762c022d2cff1e72aec555d6f9c890b
Thanhthuan020 <71782129+cabupig036@users.noreply.github.com>
2022-03-14 20:25:10 +0700

    Add master

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const shippingAdressSchema = new mongoose.Schema({ isDefault: { type: Boolean, default: false }, address: String });
-const addedPointLogSchema = new mongoose.Schema({ billID: String, addedPoint: Number });
+const addedPointLogSchema = new mongoose.Schema({ billID: String, addedPoint: Number ,createAt:{type:Date,  default: Date.now}});
 const seenItemSchema = new mongoose.Schema({ bookId: String, price: Number, amount: Number, bookName: String });
 const wishItemSchema = new mongoose.Schema({ bookId: String });
 const UserSchema = new mongoose.Schema({
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
     phone: String,
     shippingAdress: [shippingAdressSchema],
     isVipMember: { type: Boolean, default: false },
-    addedPointLogs: [addedPointLogSchema],
+    addedPointLogs: [addedPointLogSchema], 
     currentPoint: Number,
     seenList: [seenItemSchema],
     wishList: [wishItemSchema]
