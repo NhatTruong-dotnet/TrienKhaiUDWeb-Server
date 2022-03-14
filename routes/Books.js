8e9ac2ebe47c99b0d6736822070a1f2452603f3a
Thanhthuan020 <71782129+cabupig036@users.noreply.github.com>
2022-03-14 21:36:47 +0700

    Merge Master

--- routes/Books.js
+++ routes/Books.js
@@ -1,5 +1,28 @@
 const router = require("express").Router();
-const Book = require("../models/Book");
+const User = require("../models/User");
+const Books = require("../models/Book");
+const Image = require("../models/Image");
+const {
+  json
+} = require("express");
+// const session = require('express-session');
+const multer = require("multer");
+const fs = require('fs');
+const path = require('path');
+//Storage
+const Storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'img')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({
+  storage: Storage
+}).single('image');
+
 
 router.post("/insertBook", async (req, res) => {
   try {
@@ -70,33 +93,53 @@ router.get("/delete/:_id", async (req, res) => {
 });
 router.put("/updateBook/:_id", async (req, res) => {
   try {
-    Book.findOne({
-      _id: req.params._id
-    }).exec((err, book) => {
-      if (err) {
-        res.status(500).json({message:"Update Failed"});
-      } else {
-        book.name = req.body.name;
-        book.publisher = req.body.publisher;
-        book.suppiler = req.body.suppiler;
-        book.numberInStock = req.body.numberInStock;
-        book.numberDelivery = req.body.numberDelivery;
-        book.author = req.body.author;
-        book.translator = req.body.translator;
-        book.publishYear = req.body.publishYear;
-        book.bookLayout = req.body.bookLayout;
-        book.price = req.body.price;
-        book.quantityOfPage = req.body.quantityOfPage;
-        book.describe = req.body.describe;
-        book.updatedAt = req.body.updatedAt;
-        book.profilePicture = req.body.picture;
-        book.save();
+        upload(req, res, (err) => {
+          const format = new RegExp("[<>#$%.^*+*]");
+          if (format.test(req.body.name) == true ||
+            format.test(req.body.publisher) == true ||
+            format.test(req.body.suppiler) == true) {
+            return res.json({
+              message: "Thông tin không hợp lệ"
+            });
+          }
+          if (err) {
+            res.status(304).json(err);
+          } else {
+            const img = {
+              imgName: req.file.originalname,
+              image: {
+                data: fs.readFileSync(path.join('img/' + req.file.filename)),
+                contentType: 'image/jpeg'
+              }
+            }
+            Image.create(img, (err, item) => {
+              if (err) {
+                res.status(401).json(err);
+              } else {
+                item.save();
+              }
+            });
+            book.name = req.body.name;
+            book.publisher = req.body.publisher;
+            book.suppiler = req.body.suppiler;
+            book.numberInStock = req.body.numberInStock;
+            book.numberDelivery = req.body.numberDelivery;
+            book.author = req.body.author;
+            book.translator = req.body.translator;
+            book.publishYear = req.body.publishYear;
+            book.bookLayout = req.body.bookLayout;
+            book.price = req.body.price;
+            book.quantityOfPage = req.body.quantityOfPage;
+            book.describe = req.body.describe;
+            book.updatedAt = req.body.updatedAt;
+            book.save();
         return res.status(200).json({
           message: "Update Completed"
         })
-      }
+          }
     });
   } catch (error) {
+    console.log(error);
     res.status(500).json(error);
   }
 });
