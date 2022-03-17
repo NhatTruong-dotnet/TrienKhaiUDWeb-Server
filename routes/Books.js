ba0331ecaa09fd9837a60553c3dd10371e3a8a52
Thanhthuan020 <71782129+cabupig036@users.noreply.github.com>
2022-03-17 12:08:43 +0700

    insert/update/delete

--- routes/Books.js
+++ routes/Books.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const User = require("../models/User");
 const Books = require("../models/Book");
 const Image = require("../models/Image");
 const {
@@ -9,6 +8,7 @@ const {
 const multer = require("multer");
 const fs = require('fs');
 const path = require('path');
+const Book = require("../models/Book");
 //Storage
 const Storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -26,28 +26,53 @@ const upload = multer({
 
 router.post("/insertBook", async (req, res) => {
   try {
-      let newBook = {
-        name: req.body.name,
-        publisher: req.body.publisher,
-        suppiler: req.body.suppiler,
-        numberInStock: req.body.numberInStock,
-        numberDelivery: req.body.numberDelivery,
-        author: req.body.author,
-        translator: req.body.translator,
-        publishYear: req.body.publishYear,
-        bookLayout: req.body.bookLayout,
-        price: req.body.price,
-        quantityOfPage: req.body.quantityOfPage,
-        describe: req.body.describe
-
-      };
-      var newBookSaved = await Book.create(newBook);
-      res.status(200).json("Inserted");
-  } catch (error) 
-  {
-    res.status(500).json(error);
-    console.log(error);
-  }
+    upload(req, res, (err) => {
+      if (req.file.mimetype != 'image/jpg' ||
+       req.file.mimetype != 'image/jpeg' ||
+       req.file.mimetype != 'image/png') {
+        return res.json({
+          message: "Thông tin rỗng and not vailid"
+        });
+      }
+        const img = {
+          imgName: req.file.originalname,
+          image: {
+            data: fs.readFileSync(path.join('img/' + req.file.filename)),
+            contentType: 'image/png'
+          }
+        }
+        Image.create(img, (err, item) => {
+          if (err) {
+            res.status(401).json(err);
+          } else {
+            item.save();
+          }
+        });
+        let newBook = {
+          name: req.body.name,
+          publisher: req.body.publisher,
+          suppiler: req.body.suppiler,
+          numberInStock: req.body.numberInStock,
+          numberDelivery: req.body.numberDelivery,
+          author: req.body.author,
+          translator: req.body.translator,
+          publishYear: req.body.publishYear,
+          bookLayout: req.body.bookLayout,
+          price: req.body.price,
+          quantityOfPage: req.body.quantityOfPage,
+          describe: req.body.describe,
+          img : "http://localhost:3000/api/image/" + req.file.originalname,
+        };
+        var newBookSaved = Book.create(newBook);
+        return res.status(200).json({
+          message: "Inserted"
+        });
+    });
+} catch (error) 
+{
+  res.status(500).json(error);
+  console.log(error);
+}
 });
 router.get("/id/:id", async (req, res) => {
   try {
@@ -93,13 +118,21 @@ router.get("/delete/:_id", async (req, res) => {
 });
 router.put("/updateBook/:_id", async (req, res) => {
   try {
+    Book.findOne({
+      _id: req.params._id
+    }).exec((err, book) => {
+      if (err) {
+        res.json({
+          message: "Update Failed"
+        });
+      } else {
         upload(req, res, (err) => {
           const format = new RegExp("[<>#$%.^*+*]");
-          if (format.test(req.body.name) == true ||
-            format.test(req.body.publisher) == true ||
-            format.test(req.body.suppiler) == true) {
+          if (req.file.mimetype != 'image/jpg' ||
+           req.file.mimetype != 'image/jpeg' || 
+           req.file.mimetype != 'image/png') {
             return res.json({
-              message: "Thông tin không hợp lệ"
+              message: "Thông tin rỗng and not vailid"
             });
           }
           if (err) {
@@ -109,7 +142,7 @@ router.put("/updateBook/:_id", async (req, res) => {
               imgName: req.file.originalname,
               image: {
                 data: fs.readFileSync(path.join('img/' + req.file.filename)),
-                contentType: 'image/jpeg'
+                contentType: 'image/png'
               }
             }
             Image.create(img, (err, item) => {
@@ -123,20 +156,24 @@ router.put("/updateBook/:_id", async (req, res) => {
             book.publisher = req.body.publisher;
             book.suppiler = req.body.suppiler;
             book.numberInStock = req.body.numberInStock;
-            book.numberDelivery = req.body.numberDelivery;
-            book.author = req.body.author;
+            book.numberDelivery = req.body.numberDelivery
             book.translator = req.body.translator;
             book.publishYear = req.body.publishYear;
             book.bookLayout = req.body.bookLayout;
             book.price = req.body.price;
             book.quantityOfPage = req.body.quantityOfPage;
             book.describe = req.body.describe;
-            book.updatedAt = req.body.updatedAt;
+            if(req.file != undefined){
+              book.img = "http://localhost:3000/api/image/" + req.file.originalname;
+            }
             book.save();
-        return res.status(200).json({
-          message: "Update Completed"
-        })
+            return res.status(200).json({
+              message: "Update Completely"
+            });
           }
+        });
+
+      }
     });
   } catch (error) {
     console.log(error);
@@ -152,5 +189,4 @@ router.get("/review/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
 module.exports = router;
