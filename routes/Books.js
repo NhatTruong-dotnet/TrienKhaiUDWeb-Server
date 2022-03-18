a9a6cf120864401459fbe871667bd17229dda290
Thanhthuan020 <71782129+cabupig036@users.noreply.github.com>
2022-03-18 21:14:58 +0700

    update

--- routes/Books.js
+++ routes/Books.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const User = require("../models/User");
 const Books = require("../models/Book");
 const Image = require("../models/Image");
 const {
@@ -9,6 +8,7 @@ const {
 const multer = require("multer");
 const fs = require('fs');
 const path = require('path');
+const Book = require("../models/Book");
 //Storage
 const Storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -26,6 +26,7 @@ const upload = multer({
 
 router.post("/insertBook", async (req, res) => {
   try {
+<<<<<<< Updated upstream
     Book.find({
       name: req.params.name
     }).exec((err, book) => {
@@ -90,45 +91,89 @@ router.post("/insertBook", async (req, res) => {
     console.log(error);
     res.status(500).json(error);
   }
+=======
+    upload(req, res, (err) => {
+        const img = {
+          imgName: req.file.originalname,
+          image: {
+            data: fs.readFileSync(path.join('img/' + req.file.filename)),
+            contentType: 'image/png'
+          }
+        }
+        Image.create(img, (err, item) => {
+          if (err) {
+            res.status(401).json(err);
+          } else {
+            item.save();
+          }
+        });
+        let newBook = {
+          name: req.body.name,
+          publisher: req.body.publisher,
+          suppiler: req.body.suppiler,
+          numberInStock: req.body.numberInStock,
+          numberDelivery: req.body.numberDelivery,
+          author: req.body.author,
+          translator: req.body.translator,
+          publishYear: req.body.publishYear,
+          bookLayout: req.body.bookLayout,
+          price: req.body.price,
+          quantityOfPage: req.body.quantityOfPage,
+          describe: req.body.describe,
+          img : "https://serverbookstore.herokuapp.com/api/image/" + req.file.originalname,
+        };
+        var newBookSaved = Book.create(newBook);
+        return res.status(200).json({
+          message: "Inserted"
+        });
+    });
+} catch (error) 
+{
+  res.status(500).json(error);
+  console.log(error);
+}
+>>>>>>> Stashed changes
 });
 router.get("/id/:id", async (req, res) => {
   try {
-    const books = await Book.findById(req.params.id);
-    res.status(200).json(books);
-  } catch (error) {
+      const books = await Book.findById(req.params.id);
+      res.status(200).json(books); 
+    }
+ catch (error) {
     res.status(500).json(error);
   }
 });
 router.get("/:name", async (req, res) => {
   try {
-    const books = await Book.findOne({
-      name: req.params.name
-    });
-    res.status(200).json(books);
-  } catch (error) {
-    res.status(500).json(error);
+      const books = await Book.find({
+        name: req.params.name
+      });
+      res.status(200).json(books); 
+    }
+ catch (error) {
+    res.status(500).json(err);
   }
 });
 router.get("/", async (req, res) => {
   try {
-    const books = await Book.find({
+      const books = await Book.find({
 
-    });
-
-    res.status(200).json(books);
-  } catch (error) {
+      });
+      
+      res.status(200).json(books); 
+    }
+ catch (error) {
     res.status(500).json(error);
   }
 });
 router.get("/delete/:_id", async (req, res) => {
   try {
-    const books = await Book.deleteOne({
-      _id: req.params._id
-    });
-    console.log("Deleted");
-    res.status(200).json("Deleted");
-  } catch (error) {
-    res.status(500).json(err);
+      const books = await Book.deleteOne({_id: req.params._id });
+      console.log("Deleted");
+      res.status(200).json("Deleted"); 
+    }
+ catch (error) {
+    res.status(500).json(error);
   }
 });
 router.put("/updateBook/:_id", async (req, res) => {
@@ -142,6 +187,7 @@ router.put("/updateBook/:_id", async (req, res) => {
         });
       } else {
         upload(req, res, (err) => {
+<<<<<<< Updated upstream
           const format = new RegExp("[<>#$%.^*+*]");
           if (format.test(req.body.name) == true) {
             return res.json({
@@ -153,6 +199,8 @@ router.put("/updateBook/:_id", async (req, res) => {
               message: "Thông tin rỗng"
             });
           }
+=======
+>>>>>>> Stashed changes
           if (err) {
             res.status(304).json(err);
           } else {
@@ -169,19 +217,21 @@ router.put("/updateBook/:_id", async (req, res) => {
               } else {
                 item.save();
               }
-              book.name = req.body.name;
-              book.publisher = req.body.publisher;
-              book.suppiler = req.body.suppiler;
-              book.numberInStock = req.body.numberInStock;
-              book.numberDelivery = req.body.numberDelivery;
-              book.author = req.body.author;
-              book.translator = req.body.translator;
-              book.publishYear = req.body.publishYear;
-              book.bookLayout = req.body.bookLayout;
-              book.price = req.body.price;
-              book.quantityOfPage = req.body.quantityOfPage;
-              book.describe = req.body.describe;
+            });
+            book.name = req.body.name;
+            book.publisher = req.body.publisher;
+            book.suppiler = req.body.suppiler;
+            book.numberInStock = req.body.numberInStock;
+            book.numberDelivery = req.body.numberDelivery
+            book.translator = req.body.translator;
+            book.publishYear = req.body.publishYear;
+            book.bookLayout = req.body.bookLayout;
+            book.price = req.body.price;
+            book.quantityOfPage = req.body.quantityOfPage;
+            book.describe = req.body.describe;
+            if(req.file != undefined){
               book.img = "https://serverbookstore.herokuapp.com/api/image/" + req.file.originalname;
+<<<<<<< Updated upstream
               book.save();
               return res.status(200).json({
                 message: "Update Completely"
@@ -190,6 +240,16 @@ router.put("/updateBook/:_id", async (req, res) => {
             });
           }
         });
+=======
+            }
+            book.save();
+            return res.status(200).json({
+              message: "Update Completely"
+            });
+          }
+        });
+
+>>>>>>> Stashed changes
       }
     });
   } catch (error) {
@@ -199,10 +259,11 @@ router.put("/updateBook/:_id", async (req, res) => {
 });
 router.get("/review/:id", async (req, res) => {
   try {
-    const books = await Book.findById(req.params.name);
-    res.status(200).json(books.rating);
-  } catch (error) {
-    res.status(500).json(error);
+      const books = await Book.findById(req.params.name);
+      res.status(200).json(books.rating); 
+    }
+ catch (error) {
+    res.status(500).json(err);
   }
 });
 module.exports = router;
